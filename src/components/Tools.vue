21aa4aa67e281bd2710a4e3252b7b0c8a1c67ba8
mikezano <mikezano@gmail.com>
2019-10-27 21:21:55 -0700

    wip

---
+++ src/components/Tools.vue
@@ -0,0 +1,101 @@
+<template>
+	<div class="tools">
+		<input type="file" ref="file" @change="updateCanvasImage" />
+		<canvas id="canvas" ref="imageCanvas"></canvas>
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			file: null,
+		};
+	},
+	methods: {
+		updateCanvasImage(e) {
+			const self = this;
+			const files = e.target.files;
+			const reader = new FileReader();
+
+			reader.onload = () => {
+				var img = new Image();
+				img.onload = function() {
+					self.drawCanvasImage(img);
+				};
+				img.src = event.target.result;
+			};
+
+			reader.readAsDataURL(files[0]);
+
+			//this.file = this.$refs.file.files[0];
+			//console.log('file', this.file);
+		},
+
+		drawCanvasImage(img) {
+			var canvas = this.$refs.imageCanvas;
+			canvas.width = img.width;
+			canvas.height = img.height;
+
+			var ctx = canvas.getContext('2d');
+			ctx.drawImage(img, 0, 0);
+
+			var imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+			var data = imgData.data;
+			console.log(data);
+
+			for (let i = 0; i < data.length; i += 4) {
+				const red = data[i];
+				const green = data[i + 1];
+				const blue = data[i + 2];
+				const alpha = data[i + 3];
+
+				const hsl = this.rgbToHSL(red, green, blue);
+				console.log('HSL: ', hsl);
+			}
+		},
+
+		rgbToHSL(r, g, b) {
+			// Make r, g, and b fractions of 1
+			r /= 255;
+			g /= 255;
+			b /= 255;
+
+			// Find greatest and smallest channel values
+			let cmin = Math.min(r, g, b),
+				cmax = Math.max(r, g, b),
+				delta = cmax - cmin,
+				h = 0,
+				s = 0,
+				l = 0;
+
+			// Calculate hue
+			// No difference
+			if (delta == 0) h = 0;
+			// Red is max
+			else if (cmax == r) h = ((g - b) / delta) % 6;
+			// Green is max
+			else if (cmax == g) h = (b - r) / delta + 2;
+			// Blue is max
+			else h = (r - g) / delta + 4;
+
+			h = Math.round(h * 60);
+
+			// Make negative hues positive behind 360°
+			if (h < 0) h += 360;
+
+			// Calculate lightness
+			l = (cmax + cmin) / 2;
+
+			// Calculate saturation
+			s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
+
+			// Multiply l and s by 100
+			s = +(s * 100).toFixed(1);
+			l = +(l * 100).toFixed(1);
+
+			return 'hsl(' + h + ',' + s + '%,' + l + '%)';
+		},
+	},
+};
+</script>
---
+++ src/views/Beadz.vue
@@ -0,0 +1,42 @@
+<template>
+	<div class="beadz">
+		<section class="beadz__tools">
+			<Tools />
+		</section>
+		<section class="beadz__view">view</section>
+		<section class="beadz__colors">colors</section>
+	</div>
+</template>
+
+<script>
+import Tools from '../components/Tools.vue';
+export default {
+	data() {
+		return {};
+	},
+	components: {
+		Tools,
+	},
+};
+</script>
+
+<style lang="scss">
+.beadz {
+	display: flex;
+
+	&__tools,
+	&__colors {
+		width: 14rem;
+	}
+
+	&__view {
+		flex: auto;
+	}
+
+	&__tools,
+	&__colors,
+	&__view {
+		border: 1px solid blue;
+	}
+}
+</style>
